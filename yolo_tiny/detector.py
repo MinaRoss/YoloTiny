bddb214e6d80944b5137d57be25938cad8f06a1c
EinKung <forein@qq.com>
2021-01-30 14:20:49 +0800

    修改网络数据采样和侦测计算模块

--- yolo_tiny/detector.py
+++ yolo_tiny/detector.py
@@ -8,7 +8,7 @@ class Detector:
         self.net.eval()
 
     def detect(self, x_in, thresh, anchors):
-        out13, out26, cls = self.net(x_in.to(self.device))
+        out13, out26 = self.net(x_in.to(self.device))
 
         # keep boxes over thresh
         idxs26, vectors26 = self.boxFilter(out26, thresh)
@@ -18,19 +18,12 @@ class Detector:
         boxes26 = self.boxReturn(idxs26, vectors26, 16, anchors[26]).to(self.device)
         boxes13 = self.boxReturn(idxs13, vectors13, 32, anchors[13]).to(self.device)
 
-        return torch.cat([boxes26, boxes13], dim=0), self.solve_cls(cls)
-
-    def solve_cls(self, cls):
-        cls[:, 0] = (cls[:, 0] > 0.5).int()
-        cls[:, 4] = (cls[:, 4] > 0.5).int()
-        cls[cls[:, 0] == 1., 1:4] = torch.zeros([3]).to(self.device)
-        cls[cls[:, 0] != 1., 1:4] = (cls[cls[:, 0] != 1., 1:4] > 0.5).int()
-        return cls
+        return torch.cat([boxes26, boxes13], dim=0)
 
     def boxFilter(self, output, thresh):
         output = output.permute(0, 2, 3, 1)
         output = output.reshape(output.size()[0], output.size()[1],
-                                output.size()[2], 3, -1)  # N, H, W, 3, 5 (iou, cx, cy, w, h)
+                                output.size()[2], 3, -1)  # N, H, W, 3, 5 (iou, cx, cy, w, h, cls)
 
         mask = output[..., 0] > thresh
         idxs = mask.nonzero()
@@ -43,9 +36,10 @@ class Detector:
 
         # vectors [iou, cx_offset, cy_offset, w_offset, h_offset]
         anchors = torch.tensor(anchors, dtype=torch.float32).to(self.device)
-        batch, idx_cy, idx_cx, boxType = idxs[:, 0], idxs[:, 1], idxs[:, 2], idxs[:, 3]  # [N, H, W, 3(Box Type), 5]
+        batch, idx_cy, idx_cx, boxType = idxs[:, 0], idxs[:, 1], idxs[:, 2], idxs[:, 3]  # [N, H, W, 3(Box Type), 10]
         cx, cy = (idx_cx.float() + vectors[:, 1]) * scaleRate, (idx_cy.float() + vectors[:, 2]) * scaleRate
         w, h = anchors[boxType, 0] * torch.exp(vectors[:, 3]), anchors[boxType, 1] * torch.exp(vectors[:, 4])
         confi = vectors[..., 0]
+        cls = vectors[..., 5:]
 
-        return torch.stack([batch.float(), confi, cx, cy, w, h], dim=1)
+        return torch.stack([batch.float(), confi, cx, cy, w, h, cls], dim=1)
--- yolo_tiny/sampler.py
+++ yolo_tiny/sampler.py
@@ -13,9 +13,9 @@ class Sampling(Dataset):
         self.image_paths = []
         with open(label_path, 'r') as f:
             for line in f.readlines():
-                file, label = line.split()
+                file, *label = line.split()
                 self.image_paths.append(os.path.join(image_dir, file))
-                self.labels.append(label.split("."))
+                self.labels.append(list(map(lambda x: x.split('.'), label)))
         self.anchors = anchors
         self.areas = areas
 
@@ -25,21 +25,18 @@ class Sampling(Dataset):
     def __getitem__(self, index):
         labels = {}
         img_data = cv2.imread(self.image_paths[index]).transpose([2, 0, 1]).astype(np.float32) / 255 - 0.5
-        box = np.array([float(x) for x in self.labels[index][:4]])
-        cls = np.array([float(x) for x in self.labels[index][4:]], dtype=np.float32)
 
         for featureSize, sub_anchors in self.anchors.items():  # per feature size
             labels[featureSize] = np.zeros(shape=(featureSize, featureSize, 3, 5), dtype=np.float32)
+            for box in self.labels[index]:
+                cx, cy, w, h, *cls = list(map(int, box))  # centre x & y, w & h
+                cx_offset, cx_idx = math.modf(cx * featureSize / 416)
+                cy_offset, cy_idx = math.modf(cy * featureSize / 416)
+                for i, anchor in enumerate(sub_anchors):  # per box in one feature size
+                    parea = self.areas[featureSize][i]
+                    pw, ph = w / anchor[0], h / anchor[1]
+                    barea = w * h
+                    iou = min(parea, barea) / max(parea, barea)
+                    labels[featureSize][int(cy_idx), int(cx_idx), i] = np.array([iou, cx_offset, cy_offset, np.log(pw), np.log(ph), *cls])
 
-            cx, cy, w, h = box  # centre x & y, w & h
-            cx_offset, cx_idx = math.modf(cx * featureSize / 416)
-            cy_offset, cy_idx = math.modf(cy * featureSize / 416)
-            for i, anchor in enumerate(sub_anchors):  # per box in one feature size
-                parea = self.areas[featureSize][i]
-                pw, ph = w / anchor[0], h / anchor[1]
-                barea = w * h
-                iou = min(parea, barea) / max(parea, barea)
-                labels[featureSize][int(cy_idx), int(cx_idx), i] = np.array(
-                    [iou, cx_offset, cy_offset, np.log(pw), np.log(ph)])
-
-        return img_data, labels[26], labels[13], cls
+        return img_data, labels[26], labels[13]
